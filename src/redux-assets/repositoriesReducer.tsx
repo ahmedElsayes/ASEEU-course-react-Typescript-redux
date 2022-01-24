a59615f3bcfde8844e50e3cd83ef3609feaac37c
Ahmed Elsayes <mysterious.en2010@gmail.com>
2022-01-24 09:32:48 +0200

    Applying more accurate definitions for Actions

--- src/redux-assets/repositoriesReducer.tsx
+++ src/redux-assets/repositoriesReducer.tsx
@@ -3,17 +3,31 @@ interface RepositoriesState {
   error: string | null;
   data: string[]
 }
-interface Action {
-  type: string;
-  payload: any;
+// interface Action {
+//   type: string;
+//   payload: any;
+// }
+interface SearchRepositoriesRequest {
+  type: "SEARCH_REPOSITORIES_REQUEST"
 }
-const reducer = (state: RepositoriesState, action: Action): RepositoriesState => {
+interface SearchRepositoriesSuccess {
+  type: "SEARCH_REPOSITORIES_SUCCESS",
+  payload: string[]
+}
+interface SearchRepositoriesError {
+  type: "SEARCH_REPOSITORIES_ERROR",
+  payload: string
+}
+const reducer = (
+  state: RepositoriesState,
+  action: SearchRepositoriesRequest | SearchRepositoriesError | SearchRepositoriesSuccess
+  ): RepositoriesState => {
   switch (action.type) {
     case "SEARCH_REPOSITORIES_REQUEST":
       return { loading: true, error: null, data: [] }
     case "SEARCH_REPOSITORIES_SUCCESS":
       return { loading: false, error: null, data: action.payload }
-    case "SEARCH_REPOSITORIES_Error":
+    case "SEARCH_REPOSITORIES_ERROR":
       return { loading: false, error: action.payload, data: [] }
     default:
       return state
