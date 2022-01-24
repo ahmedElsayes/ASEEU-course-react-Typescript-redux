185face9e7ef825f57ed8461dde63c42d8928428
Ahmed Elsayes <mysterious.en2010@gmail.com>
2022-01-24 11:30:32 +0200

    Wiring Redux states to root component

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,22 @@ import React from 'react';
 
 function App() {
   return (
-    <div>
-      Fetching npm packages
+    <div className='container'>
+      <h2> Search for Package </h2>
+      <div className='row align-items-end mt-5'>
+        <div className='col-md-4 col-xs-12'>
+          <div>
+            <label className='mb-1'>Keyword search</label>
+            <input
+              className="form-control"
+              placeholder="Search about npm package"
+            />
+          </div>
+        </div>
+        <div className='col-md-4 col-xs-12'>
+          <button className='special-button'> Search </button>
+        </div>
+      </div>
     </div>
   );
 }
